2c2619eced05a38dc6e8fd1b6a57257087380268
sohaibaltrefe <sohaibshaltafaltrefe1@gmail.com>
2024-10-04 21:18:42 +0300

    Add project files.

---
+++ Medicio.PL/Areas/Dashboard/Controllers/CategoryController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Medicio.PL.Areas.Dashboard.Controllers
+{
+    [Area("Dashboard")]
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
